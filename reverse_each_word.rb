62f86736766717681a375023799bce5354a3e150
Michael Rabon <nobar86@gmail.com>
2020-08-03 21:22:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
-  "Hello there, and how are you?"
-  sentence.reverse
+  "Hello there, and how are you?".reverse
+  sentence.each do |sentence|
     
-  
+end  
 end
