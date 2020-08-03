c0a0223d2c8644b7a7c0d081dea20ef631a043ef
Michael Rabon <nobar86@gmail.com>
2020-08-03 22:40:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 def reverse_each_word(sentence)
   my_array = sentence.split(" ")
   returned_array = []
-  my_array.map do |string|
+  my_array.collect do |string|
     returned_array << string.reverse
   end
   returned_array.join(" ")
